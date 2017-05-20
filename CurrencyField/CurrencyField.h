c6642d4b7dd8f082a5c14e5a8c1cb56ccc857fb8
Aaren Tebbutt <aaren.tebbutt@noshjournal.com>
2017-05-20 20:30:44 +1000

    Initial Commit

---
+++ CurrencyField/CurrencyField.h
@@ -0,0 +1,19 @@
+//
+//  CurrencyField.h
+//  CurrencyField
+//
+//  Created by Aaren Tebbutt on 20/5/17.
+//  Copyright © 2017 Noshjournal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CurrencyField.
+FOUNDATION_EXPORT double CurrencyFieldVersionNumber;
+
+//! Project version string for CurrencyField.
+FOUNDATION_EXPORT const unsigned char CurrencyFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CurrencyField/PublicHeader.h>
+
+
