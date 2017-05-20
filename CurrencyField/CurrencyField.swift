474f9558bc726bf289eea50f45a83df3391ad8c2
Aaren Tebbutt <aaren.tebbutt@noshjournal.com>
2017-05-20 20:41:25 +1000

    Created the base subclass of UITextField

---
+++ CurrencyField/CurrencyField.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyField.swift
+//  CurrencyField
+//
+//  Created by Aaren Tebbutt on 20/5/17.
+//  Copyright © 2017 Noshjournal. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyField: UITextField {
+    
+}
