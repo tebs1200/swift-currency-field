57de60f3b9daebbff19abbf345e20af1461328a8
Aaren Tebbutt <aaren.tebbutt@noshjournal.com>
2017-05-21 18:10:36 +1000

    Made textDidChange public so that the selector will work

--- CurrencyField/CurrencyField.swift
+++ CurrencyField/CurrencyField.swift
@@ -52,7 +52,7 @@ public class CurrencyField: UITextField {
         return end
     }
     
-    func textDidChange() {
+    public func textDidChange() {
         
         if let currentText = self.text, currentText != "" {
             
